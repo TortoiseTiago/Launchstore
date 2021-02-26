fe7b1ecc0eac75f9b24173d89ebb4fc1ab819a85
TortoiseTiago <tortoise.tl@gmail.com>
2021-02-25 23:16:22 -0300

    initial part of product registration

--- src/app/controllers/product.js
+++ src/app/controllers/product.js
@@ -1,5 +1,35 @@
+const Category = require("../models/Category");
+const Product = require("../models/Product");
+
 module.exports = {
   create(req, res) {
-    return res.render("./products/create.njk");
+    // Pegar as categorias
+    Category.all()
+      .then(function (results) {
+        const categories = results.rows;
+        return res.render("./products/create.njk", { categories });
+      })
+      .catch(function (err) {
+        throw new Error(err);
+      });
+    // feito com .then().catch() apenas para entendimento da evolução do codigo
+  },
+  async post(req, res) {
+    // Logica para salvar
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields");
+      }
+    }
+
+    let results = await Product.create(req.body);
+    const productId = results.rows[0].id;
+
+    results = await Category.all();
+    const categories = results.rows;
+
+    return res.render("/products/create.njk", { product, categories });
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,9 @@ routes.get("/", (req, res) => {
 });
 
 routes.get("/products/create", ProductController.create);
+routes.post("/products", ProductController.post);
 
+//  Alias = Atalhos
 routes.get("/ads/create", (req, res) => {
   return res.redirect("/products/create");
 });
